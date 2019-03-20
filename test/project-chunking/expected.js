4e5a6f84c78a7ebf8605b5f5038673f55eddb46c
Guy Bedford <guybedford@gmail.com>
2019-03-20 17:20:11 +0200

    handle timing / platform variations in project test

--- test/project-chunking/expected.js
+++ test/project-chunking/expected.js
@@ -2,11 +2,8 @@
 const fs = require('fs');
 
 expect(output.length).toBe(18);
-expect(output[5].trim().substr(0, 9)).toBe("asset.txt");
-expect(output[6].trim().substr(0, 10)).toBe("asset1.txt");
-expect(output[7].trim().substr(0, 10)).toBe("asset3.txt");
 
 // check relative asset references worked out
-expect(fs.readFileSync(__dirname + "/dist/modules/main.js").toString()).toContain(`"/../asset.txt"`);
-expect(fs.readFileSync(__dirname + "/dist/modules/chunk.js").toString()).toContain(`"/../asset1.txt"`);
-expect(fs.readFileSync(__dirname + "/dist/modules/chunks/2.js").toString()).toContain(`"/../../asset3.txt"`);
+expect(fs.readFileSync(__dirname + "/dist/modules/main.js").toString()).toContain(`"/../asset`);
+expect(fs.readFileSync(__dirname + "/dist/modules/chunk.js").toString()).toContain(`"/../asset`);
+expect(fs.readFileSync(__dirname + "/dist/modules/chunks/2.js").toString()).toContain(`"/../../asset`);
