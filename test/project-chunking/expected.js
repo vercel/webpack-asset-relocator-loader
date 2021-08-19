64a40fb046cb9528dd71e2c8e49b160cf236d567
Guy Bedford <guybedford@gmail.com>
2021-08-18 20:43:27 -0700

    update test

--- test/project-chunking/expected.js
+++ test/project-chunking/expected.js
@@ -6,4 +6,4 @@ expect(output.length).toBe(16);
 // check relative asset references worked out
 expect(fs.readFileSync(__dirname + "/dist/modules/main.js").toString()).toContain(`ab+"asset`);
 expect(fs.readFileSync(__dirname + "/dist/modules/chunk.js").toString()).toContain(`ab+"asset`);
-expect(fs.readFileSync(__dirname + "/dist/modules/chunks/541.js").toString()).toContain(`ab+"asset`);
+expect(fs.readFileSync(__dirname + "/dist/modules/chunks/758.js").toString()).toContain(`ab+"asset`);
