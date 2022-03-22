0f985a26afdb0a3d28f601f75e34647fb15073a9
Steven <steven@ceriously.com>
2022-03-22 19:37:53 -0400

    Fix tests on windows

--- test/project.test.js
+++ test/project.test.js
@@ -1,16 +1,18 @@
 const fs = require("fs");
-const child_process = require("child_process");
+const { join } = require("path");
+const { execSync } = require("child_process");
 
 for (const project of fs.readdirSync(__dirname).filter(name => name.startsWith("project-"))) {
-  it(`should correctly run webpack build ${project}`, async () => {
-    const stdout = child_process.execSync(__dirname + "/../node_modules/.bin/webpack -c webpack.config.js", {
-      cwd: __dirname + "/" + project
-    });
+  it(`should correctly run webpack build ${project}`, async () => { 
+    const webpack = join(__dirname, '..', 'node_modules', '.bin', 'webpack');
+    const cwd = join(__dirname, project);
+    const command = `${webpack} -c webpack.config.js`;
+    const stdout = execSync(command, { cwd });
 
     const output = stdout.toString().split("\n");
     ((output, __dirname) => {
       output;
-      eval(fs.readFileSync(__dirname + "/expected.js").toString());
-    })(output, __dirname + "/" + project);
+      eval(fs.readFileSync(join(__dirname, 'expected.js')).toString());
+    })(output, cwd);
   });
 }
