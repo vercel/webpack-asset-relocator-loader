5de3afbbc53ff5490fcce3db84dd21cf19b7bcc8
Steven <steven@ceriously.com>
2025-07-03 10:49:58 -0400

    chore(ci): remove codecov

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   collectCoverageFrom: ["src/**/*.js"],
   coverageReporters: ["html", "lcov"],
+    coverageThreshold: {
+    global: {
+      branches: 99,
+      functions: 99,
+      lines: 99,
+    },
+  },
   testEnvironment: "node",
   testMatch: ["<rootDir>/test/*(*.)+(spec|test).js?(x)"]
 };
