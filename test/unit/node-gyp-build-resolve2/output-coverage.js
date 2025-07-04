d15a3fab2f22ff5dcf5841f856fa9f504b7e5886
Yeoh Joer <yeoh.joer@outlook.com>
2025-07-04 11:18:01 +0800

    feat: support loading node native addons from the prebuilds directory (#198)
    Extracted from #194
    
    This is taking advantage of #185, which used a function in
    node-gyp-build to compute the path. Some moons ago,
    [node-gyp-build.js#L62-L75](https://github.com/prebuild/node-gyp-build/blob/v4.8.4/node-gyp-build.js#L62-L75)
    was updated with a function to resolve native addons from the prebuilds
    directory. All we have to do here is update `node-gyp-build`.
    
    This enables assets relocation for packages like [bcrypt
    v6.0.0](https://www.npmjs.com/package/bcrypt?activeTab=code)
    
    I added some tests according to the platform used in CI. Hopefully, the
    architecture and the fixtures align well.
    
    ---------
    
    Co-authored-by: Steven <steven@ceriously.com>

---
+++ test/unit/node-gyp-build-resolve2/output-coverage.js
@@ -0,0 +1,61 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 257:
+/***/ ((__unused_webpack_module, __unused_webpack_exports, __webpack_require__) => {
+
+const path = __webpack_require__(17);
+require(__webpack_require__.ab + "node.napi.node");
+
+
+/***/ }),
+
+/***/ 17:
+/***/ ((module) => {
+
+"use strict";
+module.exports = require("path");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/asset-relocator-loader */
+/******/ 	if (typeof __webpack_require__ !== 'undefined') __webpack_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__(257);
+
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
