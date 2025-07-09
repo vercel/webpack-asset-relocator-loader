009363176aaea7a97b94807ad1ed94b3d76cc826
Yeoh Joer <yeoh.joer@outlook.com>
2025-07-10 02:17:37 +0800

    test: assert the number of chunks and emitted assets

--- test/project-chunking/expected.js
+++ test/project-chunking/expected.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 
 expect(stats.compilation.errors.length).toBe(0);
+expect(stats.compilation.chunks.length).toBe(8);
 
 // check relative asset references worked out
 expect(fs.readFileSync(__dirname + "/dist/main.js").toString()).toContain(`ab+"asset`);
@@ -11,3 +12,7 @@ expect(fs.readFileSync(__dirname + "/dist/chunks/42.js").toString()).toContain(`
 
 expect(fs.readFileSync(__dirname + "/dist/chunks/sharp-chunk.js").toString()).toContain(`ab+"sharp`);
 expect(fs.readFileSync(__dirname + "/dist/chunks/sharp32-chunk.js").toString()).toContain(`ab+"build`);
+
+const assets = Object.keys(stats.compilation.assets);
+expect(assets.some(asset => /lib\/sharp-.+\.node/.exec(asset))).toBeTruthy();
+expect(assets.some(asset => /Release\/sharp-.+\.node/.exec(asset))).toBeTruthy();
